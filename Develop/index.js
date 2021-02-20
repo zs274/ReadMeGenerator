20440de29eccbea7545376b05363bf27ccf1a896
Zarin Salim <zarin_96@hotmail.co.uk>
2021-02-20 17:09:25 +0000

    added require fs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const markdown = require('./utils/generateMarkdown');
-
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = ["Title of your project", "Describe your project", "How to install your project", "How to use your project", "How to contribute to your project", "How to test your project", "License of your project", "Github username", 'Github link', "Email address"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile
 
 }
 
