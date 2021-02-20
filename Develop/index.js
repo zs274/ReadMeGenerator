32688039d922e477e60179dd036862b847b7b274
Zarin Salim <zarin_96@hotmail.co.uk>
2021-02-20 03:09:02 +0000

    formatted document

--- Develop/index.js
+++ Develop/index.js
@@ -13,64 +13,54 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt([{
-        type: 'input',
-        message: questions[0],
-        name: 'title'
-    }, 
-    {
-        type: 'input',
-        message: questions[1],
-        name: 'description'
-    }, 
-    {
-        type: 'input',
-        message: questions[2],
-        name: 'installation'
-    }, 
-    {
-        type: 'input',
-        message: questions[3],
-        name: 'usage'
-    }, 
-    {
-        type: 'input',
-        message: questions[4],
-        name: 'contributions'
-    }, 
-    {
-        type: 'input',
-        message: questions[5],
-        name: 'test'
-    }, 
-    {
-        type: 'list',
-        message: questions[6],
-        name: 'license',
-        choices: ['MIT', 'Apache', 'GPL', 'Other' ]
-    }, 
-    {
-        type: 'input',
-        message: questions[7],
-        name: 'github username'
-    }, 
-    {
-        type: 'input',
-        message: questions[8],
-        name: 'email'
-    }, 
-    ])
+        .prompt([{
+            type: 'input',
+            message: questions[0],
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'contributions'
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'test'
+        },
+        {
+            type: 'list',
+            message: questions[6],
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPL', 'Other']
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'github username'
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email'
+        },
+        ])
 }
 
 // Function call to initialize app
 init();
-
-
-// var inquirer = require('inquirer');
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   });
