d471f71faf1104c65f1a781a037c46a6f2c33911
Zarin Salim <zarin_96@hotmail.co.uk>
2021-02-20 03:57:32 +0000

    added question asking for github link, edited init function to reflect changes

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
-const questions = ["Title of your project", "Describe your project", "How to install your project", "How to use your project", "How to contribute to your project", "How to test your project", "License of your project", "Github username", "Email address"];
+const questions = ["Title of your project", "Describe your project", "How to install your project", "How to use your project", "How to contribute to your project", "How to test your project", "License of your project", "Github username", 'Github link', "Email address"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -52,11 +52,16 @@ function init() {
         {
             type: 'input',
             message: questions[7],
-            name: 'github username'
+            name: 'gitusername'
         },
         {
             type: 'input',
             message: questions[8],
+            name: 'gitlink'
+        },
+        {
+            type: 'input',
+            message: questions[9],
             name: 'email'
         },
         ])
