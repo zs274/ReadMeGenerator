84b854028bd7e754140396d396776d011a98f4de
Zarin Salim <zarin_96@hotmail.co.uk>
2021-02-20 01:30:41 +0000

    created question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Title of your project", "Describe your project", "How to install your project", "How to use your project", "How to contribute to your project", "How to test your project", "License of your project", "Github username", "Email address"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
