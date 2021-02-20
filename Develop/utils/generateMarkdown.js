cb23c41b8995f5dbd555d72a0d6e5801280a51f7
Zarin Salim <zarin_96@hotmail.co.uk>
2021-02-20 03:09:25 +0000

    created function to display license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache':
+      return '![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'GPL':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+      default:
+      "";
+      break;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
